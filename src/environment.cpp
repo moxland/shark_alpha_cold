0c0d0a80639ae6f424827bda7aa5a8df6b68098a
Megan Oxland <26727410+moxland@users.noreply.github.com>
2025-04-10 14:54:35 -0400

    Update environment.cpp
    Changed location of if satellite_subhalo.hot_halo_gas_r_rps == 0

--- src/environment.cpp
+++ src/environment.cpp
@@ -113,6 +113,11 @@ void Environment::process_satellite_subhalo_environment(Subhalo &satellite_subha
 				auto func_rvir = ram_pressure_stripping_hot_gas(central_subhalo, satellite_subhalo, satellite_subhalo.rvir_infall, z, ram_press);
 				auto func_rvirdiv100 = ram_pressure_stripping_hot_gas(central_subhalo, satellite_subhalo, satellite_subhalo.rvir_infall/100, z, ram_press);
 
+				if(satellite_subhalo.hot_halo_gas_r_rps == 0){
+					// if the hot_halo_gas_r_rps is initialized to 0, instead change it to rvir_infall.
+					satellite_subhalo.hot_halo_gas_r_rps = satellite_subhalo.rvir_infall;
+				}
+				
 				if(func_rvir > 0){
 					r_rps = satellite_subhalo.hot_halo_gas_r_rps;
 				}
@@ -122,10 +127,7 @@ void Environment::process_satellite_subhalo_environment(Subhalo &satellite_subha
 				else if (func_rvir < 0 && func_rvirdiv100 < 0){
 					r_rps = 0;
 				}
-				if(satellite_subhalo.hot_halo_gas_r_rps == 0){
-					// if the hot_halo_gas_r_rps is initialized to 0, instead change it to rvir_infall.
-					satellite_subhalo.hot_halo_gas_r_rps = satellite_subhalo.rvir_infall;
-				}
+				
 				// If the ram-pressure stripping radius has decreased from previous timesteps, then compute how much new gas is lost.
 				if(r_rps < satellite_subhalo.hot_halo_gas_r_rps && r_rps > 0){
 					// 1. compute hot gas outside r_rps
